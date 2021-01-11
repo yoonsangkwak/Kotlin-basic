56bdfd3ae600026a779ab0b0e2dda941e0b0222b
yoonsangkwak <yunsang08@naver.com>
2021-01-11 21:09:02 +0900

    Context

---
+++ ViewComponent/app/src/main/java/com/example/viewcomponent/Context.kt
@@ -0,0 +1,14 @@
+package com.example.viewcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Context : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_context)
+
+        val context: Context = this
+        val applicationContext = getApplicationContext()
+    }
+}
