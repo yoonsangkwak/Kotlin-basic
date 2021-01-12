204c2e570742e66b220b35622ec779c30d5a9a98
yoonsangkwak <yunsang08@naver.com>
2021-01-12 16:30:07 +0900

    PhoneBook

---
+++ ViewComponent/app/src/main/java/com/example/viewcomponent/PhoneBookActivity.kt
@@ -0,0 +1,63 @@
+package com.example.viewcomponent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class PhoneBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_book)
+
+        val phoneBook = createFakePhoneBook(30)
+        createPhoneBookList(phoneBook)
+    }
+
+    fun createFakePhoneBook(fakeNumber: Int = 10, phoneBook: PhoneBook = PhoneBook()): PhoneBook {
+        for (i in 0 until fakeNumber) {
+            val j: String = if (i < 10) "0" + i else i.toString()
+            phoneBook.addPerson(
+                Person(name = "홍길동" + i, number = "010-1234-56" + j)
+            )
+        }
+        return phoneBook
+    }
+
+    fun createPhoneBookList(phoneBook: PhoneBook) {
+        val layoutInflater = LayoutInflater.from(this@PhoneBookActivity)
+        val container = findViewById<LinearLayout>(R.id.phonebook_list_container)
+        for (i in 0 until phoneBook.personList.size) {
+            val view = layoutInflater.inflate(R.layout.item_phone_book, null)
+            val personNameView = view.findViewById<TextView>(R.id.person_name)
+            personNameView.setText(phoneBook.personList.get(i).name)
+            addSetOnClickListener(phoneBook.personList.get(i), view)
+            container.addView(view)
+        }
+    }
+
+    fun addSetOnClickListener(person: Person, view: View) {
+        view.setOnClickListener {
+            val intent = Intent(this@PhoneBookActivity, PhoneBookDetailActivity::class.java)
+            intent.putExtra("name", person.name)
+            intent.putExtra("number", person.number)
+            startActivity(intent)
+        }
+    }
+}
+
+class PhoneBook() {
+    // 전화번호부
+    val personList = ArrayList<Person>()
+
+    fun addPerson(person: Person) {
+        personList.add(person)
+    }
+}
+
+class Person(val name: String, var number: String) {
+
+}
