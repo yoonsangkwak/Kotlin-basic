afa7098cb1f140d37ec950f5b6573a7335249a8c
yoonsangkwak <yunsang08@naver.com>
2021-01-13 21:51:46 +0900

    Library Realm

---
+++ ViewComponent/app/src/main/java/com/example/viewcomponent/RealmActivity.kt
@@ -0,0 +1,45 @@
+package com.example.viewcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import kotlinx.android.synthetic.main.activity_realm.*
+
+class RealmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realm)
+
+        Realm.init(this@RealmActivity)
+        val config: RealmConfiguration = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(config)
+        val realm = Realm.getDefaultInstance()
+
+        button_save.setOnClickListener {
+            realm.executeTransaction {
+                with(it.createObject(School::class.java)) {
+                    this.name = "어떤 대학교"
+                    this.location = "서울"
+                }
+            }
+        }
+
+        button_load.setOnClickListener {
+            realm.executeTransaction {
+                val data = it.where(School::class.java).findFirst()
+                Log.d("data-a", "data : " + data)
+            }
+        }
+
+        button_delete.setOnClickListener {
+            realm.executeTransaction {
+                it.where(School::class.java).findAll().deleteAllFromRealm()
+//                it.where(School::class.java).findFirst().deleteFromRealm() -> 첫번째만 지우기
+            }
+        }
+    }
+}
