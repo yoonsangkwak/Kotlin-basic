753ba07419f1fc916bebabf6c612ee968430953b
yoonsangkwak <yunsang08@naver.com>
2021-03-29 18:02:29 +0900

    databinding

--- ModernAndroid/app/src/main/java/site/yoonsang/modernandroid/MainActivity.kt
+++ ModernAndroid/app/src/main/java/site/yoonsang/modernandroid/MainActivity.kt
@@ -2,35 +2,24 @@ package site.yoonsang.modernandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import site.yoonsang.modernandroid.databinding.ActivityMainBinding
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.lifecycleOwner = this // LiveData를 활용하기 위해서
 
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-        viewModel.getAll().observe(this, Observer { todos ->
-            binding.resultText.text = todos.toString()
-        })
-
-        binding.btnAdd.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO) {
-                viewModel.insert(Todo(binding.editTodo.text.toString()))
-            }
-        }
+        binding.viewModel = viewModel
     }
 }
--- ModernAndroid/app/src/main/java/site/yoonsang/modernandroid/MainViewModel.kt
+++ ModernAndroid/app/src/main/java/site/yoonsang/modernandroid/MainViewModel.kt
@@ -3,19 +3,32 @@ package site.yoonsang.modernandroid
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel(application: Application): AndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val db = Room.databaseBuilder(
         application,
         AppDatabase::class.java, "database-name"
     ).build()
 
+    var todos: LiveData<List<Todo>>
+
+    var newTodo: String? = null
+
+    init {
+        todos = getAll()
+    }
+
     fun getAll(): LiveData<List<Todo>> {
         return db.todoDao().getAll()
     }
 
-    suspend fun insert(todo: Todo) {
-        db.todoDao().insert(todo)
+    fun insert(todo: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            db.todoDao().insert(Todo(todo))
+        }
     }
 }
