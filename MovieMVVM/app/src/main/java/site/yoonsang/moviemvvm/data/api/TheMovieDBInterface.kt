5f8a7537a940d7dadc798424343d2fc0889dba8e
Yoonsang Kwak <yunsang08@naver.com>
2021-04-28 16:27:32 +0900

    Update TheMovieDBInterface.kt

--- MovieMVVM/app/src/main/java/site/yoonsang/moviemvvm/data/api/TheMovieDBInterface.kt
+++ MovieMVVM/app/src/main/java/site/yoonsang/moviemvvm/data/api/TheMovieDBInterface.kt
@@ -9,10 +9,6 @@ import site.yoonsang.moviemvvm.data.vo.MovieResponse
 
 interface TheMovieDBInterface {
 
-    // https://api.themoviedb.org/3/movie/popular?api_key=ce93d459c0e647dd2bc8bc9834eae6a3
-    // https://api.themoviedb.org/3/movie/399566?api_key=ce93d459c0e647dd2bc8bc9834eae6a3
-    // https://api.themoviedb.org/3/
-
     @GET("/3/movie/popular")
     fun getPopularMovie(
         @Query("page") page: Int
